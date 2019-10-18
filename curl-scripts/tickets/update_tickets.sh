c742c8e3c7ca094e49ec3dfa1cba1b7708a3e5c4
Nancy4510 <nancy4510@gmail.com>
2019-10-18 10:00:37 -0400

    project2Api
    I was able to create an item through the terminal and in the browser.

--- curl-scripts/tickets/update_tickets.sh
+++ curl-scripts/tickets/update_tickets.sh
@@ -2,7 +2,7 @@ curl --include --request PATCH "http://localhost:4741/tickets/${ID}" \
   --header 'Content-Type: application/json' \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "tickets": {
+    "ticket": {
       "date": "'"${DATE}"'",
       "description": "'"${DESCRIPTION}"'"
     }
